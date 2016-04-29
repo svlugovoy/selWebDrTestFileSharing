6994dfbc0da82c2cccb7fcb6bba2ecafe1c3bff0
svlugovoy <svlugovoy@gmail.com>
2016-04-29 23:32:22 +0300

    initial commit

---
+++ src/main/java/com/svlugovoy/App.java
@@ -0,0 +1,13 @@
+package com.svlugovoy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
