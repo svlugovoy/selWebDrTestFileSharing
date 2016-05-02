4f0d4511c6ace61c99f7f21233a7cfacf5cddcb0
svlugovoy <svlugovoy@gmail.com>
2016-05-02 21:11:56 +0300

    plan of test home and profile pages

---
+++ src/test/java/com/svlugovoy/filesharing/HomePageTest.java
@@ -0,0 +1,18 @@
+package com.svlugovoy.filesharing;
+
+/**
+ * @author Sergey Lugovoy <svlugovoy@gmail.com> 02.05.2016.
+ */
+public class HomePageTest {
+
+    // 1) folder "File Sharing" is exist
+
+    // 2) create/delete new folder
+
+    // 3) upload file Drag and drop / delete
+
+    // 4) upload file (use advanced uploader) / delete
+
+    // 5) test search form
+
+}
---
+++ src/test/java/com/svlugovoy/filesharing/ProfilePageTest.java
@@ -0,0 +1,13 @@
+package com.svlugovoy.filesharing;
+
+/**
+ * @author Sergey Lugovoy <svlugovoy@gmail.com> 02.05.2016.
+ */
+public class ProfilePageTest {
+
+    // 1) change User Information and avatar
+
+    // 2) change avatar from home page
+
+
+}
